c807554f8bcc2949cab4fd8ffc92768feb99b2df
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-12-07 13:04:31 +0100

    Final commit Made by isma

--- src/app/plana-principal/plana-principal.component.ts
+++ src/app/plana-principal/plana-principal.component.ts
@@ -17,7 +17,7 @@ export class PlanaPrincipalComponent {
   tiempoRestante: number = 10000;
 
   constructor(private cdRef: ChangeDetectorRef, private ngZone: NgZone) {
-    this.socket = io("http://192.168.16.200:8888", { transports: ['websocket'], key: 'angular-client' });
+    this.socket = io("http://169.254.180.117:8888", { transports: ['websocket'], key: 'angular-client' });
 
     this.socket.on("hello", (arg: any) => {
       console.log(arg);
@@ -51,33 +51,18 @@ export class PlanaPrincipalComponent {
     this.socket.on("VerifiedCorrectly", (arg: boolean) => {
       video.verified = arg;
 
-      setTimeout(() => {
-        document.getElementById('verifyDiv')!.style.display = 'none';
-        this.resetearProgreso();
-      }, 10000);
-
-      setTimeout(() => {
-        document.getElementById('verifyErrorDiv')!.style.display = 'none';
-        this.resetearProgreso();
-      }, 10000);
-
-      const interval = 100;
-
-      let updateProgress = setInterval(() => {
-        this.tiempoRestante -= interval;
-
-        if (this.tiempoRestante <= 0) {
-          clearInterval(updateProgress);
-          this.tiempoRestante = 10000;
-          this.progreso = 100;
-          this.ngZone.run(() => {
-            this.resetearProgreso();
-          });
-        } else {
-          this.progreso = (this.tiempoRestante / 10000) * 100;
-          this.cdRef.detectChanges();
-        }
-      }, interval);
+      if(video.verified) {
+        setTimeout(() => {
+          document.getElementById('verifyDiv')!.style.display = 'none';
+          this.resetearProgreso();
+        }, 5000);
+      }
+      else {
+        setTimeout(() => {
+          document.getElementById('verifyErrorDiv')!.style.display = 'none';
+          this.resetearProgreso();
+        }, 5000);
+      }
     });
   }
 
