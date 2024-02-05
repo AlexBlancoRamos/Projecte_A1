324784c21a29c6c2cc02d50e19cd4a53881b6c1d
MarcRubioo <marc.rubio@institutvidreres.cat>
2024-02-05 11:55:48 +0100

    formualrio con estilo, ruta por defecto en el login, i redirigrdesde el boton de login a la pagina principal

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RouterOutlet} from "@angular/router";
 import {PlanaPrincipalComponent} from "./plana-principal/plana-principal.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes =[
-  {path: '', redirectTo: '/inici', pathMatch: 'full'},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'inici', component: PlanaPrincipalComponent},
   {path: 'oulet', component: RouterOutlet},
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {
+  }
+
   usuario = { correo: '', contrasena: '' };
 
   enviarFormulario() {
+    this.router.navigate(['/inici']);
     console.log('Formulario enviado', this.usuario);
   }
 
