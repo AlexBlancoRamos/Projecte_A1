01259735edfc28f93ba361c180c70c3cf41f48e4
MarcRubioo <marc.rubio@institutvidreres.cat>
2024-02-19 13:12:16 +0100

    login funcionalidad

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -7,15 +8,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuari: string='';
+  passLog: string='';
 
-  constructor(private router: Router) {
+
+  constructor(private router: Router, private http: HttpClient) {
   }
 
-  usuario = { correo: '', contrasena: '' };
+  login($myParam: string = ''): void {
+    const nav: string[] = ['']
+    if ($myParam.length) {
+      nav.push($myParam);
+    }
+    this.router.navigate(nav);
 
-  enviarFormulario() {
-    this.router.navigate(['/inici']);
-    console.log('Formulario enviado', this.usuario);
+    console.log("login")
+
+    const datos = {
+      email: this.usuari,
+      passw: this.passLog
+    };
+
+    console.log({datos})
+
+
+    this.http.post<any>('http://localhost:3000/login', datos).subscribe(
+      response => {
+        if (response.success) {
+          alert("Inicio de sesión exitoso");
+          localStorage.setItem('email', this.usuari);
+          this.router.navigate(['/inici']);
+        } else {
+          alert("Usuario y/o contraseña incorrectos");
+        }
+      },
+      error => {
+        console.error('Error al iniciar sesión:', error);
+      }
+    );
   }
 
 }
+
+
